751906b1682a050f1dba2cd0abcd9b835b866771
Ashton <ashtoncollins99@gmail.com>
2026-05-06 16:37:40 -0400

    update to turn off debug in app.py

--- app/app.py
+++ app/app.py
@@ -16,5 +16,4 @@ def pay():
     return jsonify({"message": "received", "amount": data["amount"]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
