2e99ea1b1ca1acacf9a090bf178ecb2ff0f3d417
Ashton <ashtoncollins99@gmail.com>
2026-05-06 15:11:58 -0400

    updated main,outputs etc

---
+++ scripts/user_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum update -y
+yum install -y python3 git
+
+cd /home/ec2-user
+
+# clone your repo
+git clone https://github.com/YOUR_USERNAME/payment-app.git
+
+cd payment-app/app
+
+# install dependencies
+pip3 install flask
+
+# run app
+nohup python3 app.py > app.log 2>&1 &
