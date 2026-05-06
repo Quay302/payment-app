dddfb45caf963dad1a6c5e599b23bacdb1fcb331
Ashton <ashtoncollins99@gmail.com>
2026-05-06 15:30:47 -0400

    Fix terraform file tracking

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "flask_sg" {
+  name = "flask-sg"
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "flask_server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.flask_sg.id]
+
+  user_data = file("${path.module}/../../../scripts/user_data.sh")
+
+  tags = {
+    Name = "FlaskServer"
+  }
+}
