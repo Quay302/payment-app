e69f388541458b5a9379d95e9e49d63711552645
Ashton <ashtoncollins99@gmail.com>
2026-05-06 20:56:31 -0400

    Fix main.tf to accept port 80

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,15 +6,22 @@ resource "aws_security_group" "flask_sg" {
   name = "flask-sg"
 
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
